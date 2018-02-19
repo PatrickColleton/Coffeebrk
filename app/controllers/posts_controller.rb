cf597b5dfd7594431325687d7ec7cee319e5eb80
PatrickColleton <patrickcolleton@gmail.com>
2018-02-19 14:48:00 -0500

    final commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to @post
+    redirect_to posts_path
   end
 
   def destroy
